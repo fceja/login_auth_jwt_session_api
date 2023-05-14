aec4eb84d2cbfc7a3af46a52c6ca2dd3cf519bb4
FC <fceja@FCs-MacBook-Pro.lan>
2023-05-14 11:59:20 -0700

    update /user route to use userRouter

---
+++ app/routes/user.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+import { createUser } from "../middleware/createUser";
+import { getUsers } from "../middleware/getUsers";
+
+const usersRouter = express.Router();
+
+// GET
+usersRouter.get('/getUsers', getUsers);
+
+// POST
+usersRouter.post('/create', createUser);
+
+export default usersRouter;
