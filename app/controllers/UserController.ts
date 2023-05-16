ab91c669f259203cbc430aacb237a714cde3a6b0
fceja <cisco_ceja@icloud.com>
2023-05-16 14:22:35 -0700

    add cookie session

--- app/controllers/UserController.ts
+++ app/controllers/UserController.ts
@@ -1,73 +1,92 @@
 import dbPool from "../utils/dbInit";
 
+const authConfig = require("../config/authConfig");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-export default class UserController {
-  static async createUser(user) {
-    // init db connection
-    const dbConn = await dbPool.connect();
+exports.createUserCtrl = async (user) => {
+  // init db connection
+  const dbConn = await dbPool.connect();
 
-    // check if email alrady exists
-    const emailExists = await UserController.ifEmailExists(dbConn, user.email);
-    if (emailExists) {
-      dbConn.end();
+  // check if email exists
+  const emailExists = await exports.ifEmailExists(dbConn, user.email);
+  if (emailExists) {
+    // email already exists, terminate db connection
+    dbConn.end();
 
-      return null;
-    }
-
-    try {
-      // create account in db
-      const query = await dbConn.query(
-        ` insert into "Users" (email, password, "createdAt", "lastUpdated")
-        values ($1, $2, $3, $4)
-        on conflict (email) do nothing
-        returning id, email, "createdAt", "lastUpdated"
-        `,
-        [
-          user.email,
-          bcrypt.hashSync(user.password, 10),
-          user.createdAt,
-          user.lastUpdated,
-        ]
-      );
-
-      dbConn.end();
-
-      return query.rows;
-    } catch (err) {
-      console.error(`Error: ${err}`);
-    }
+    return null;
   }
 
-  static async loginUser(user) {
-    // init db connection
-    const dbConn = await dbPool.connect();
-
-    // check if email alrady exists
-    const emailExists = await UserController.ifEmailExists(dbConn, user.email);
-    if (!emailExists) {
-      dbConn.end();
-
-      return null;
-    }
-
+  try {
+    // create account in db
     const query = await dbConn.query(
-      `select * from "Users" where email='${user.email}'`
+      ` insert into "Users" (email, password, "createdAt", "lastUpdated")
+      values ($1, $2, $3, $4)
+      on conflict (email) do nothing
+      returning id, email, "createdAt", "lastUpdated"
+      `,
+      [
+        user.email,
+        bcrypt.hashSync(user.password, 10),
+        user.createdAt,
+        user.lastUpdated,
+      ]
     );
 
-    return bcrypt.compareSync(user.password, query.rows[0].password);
+    // terminate db connection
+    dbConn.end();
+
+    return query.rows;
+
+  } catch (err) {
+    console.error(`Error: ${err}`);
+  }
+};
+
+exports.loginUserCtrl = async (user, req, res) => {
+  // init db connection
+  const dbConn = await dbPool.connect();
+
+  // check if email already exists
+  const emailExists = await exports.ifEmailExists(dbConn, user.email);
+  if (!emailExists) {
+    // email exists, terminate db connection
+    dbConn.end();
+
+    return false;
   }
 
-  static async ifEmailExists(dbConn, email) {
-    try {
-      // retrieve email if exists
-      const query = await dbConn.query(
-        `select * from "Users" where email='${email}'`
-      );
+  // query db with user email
+  const query = await dbConn.query(
+    `select * from "Users" where email='${user.email}'`
+  );
 
-      return query.rowCount > 0 ? true : false;
-    } catch (err) {
-      console.error(`Error: ${err}`);
-    }
+  // check if password is valid
+  const isPassValid = bcrypt.compareSync(user.password, query.rows[0].password);
+  if (!isPassValid) {
+
+    return false
   }
-}
+
+  // assign jwt token
+  const token = jwt.sign({ id: user.id }, authConfig.secret, {
+    expiresIn: 30, // 30 mins
+  });
+  req.session.token = token;
+
+  return true
+};
+
+exports.ifEmailExists = async (dbConn, email) => {
+  try {
+    // retrieve email from db if exists
+    const query = await dbConn.query(
+      `select * from "Users" where email='${email}'`
+    );
+
+    return query.rowCount > 0 ? true : false;
+
+  } catch (err) {
+    console.error(`Error: ${err}`);
+  }
+};
--- app/server.ts
+++ app/server.ts
@@ -1,13 +1,29 @@
+require('dotenv').config();
+
 import express, { Application } from "express";
 import bodyParser from "body-parser";
 
 import router from "./routes/routes";
 
-const app: Application = express();
+const cookieSession = require("cookie-session");
+const cors = require("cors");
 const port = 3000;
+
+const app: Application = express();
+
+app.use(cors());
 
 app.use(bodyParser.json());
 
+app.use(
+  cookieSession({
+    name: "app-session",
+    secret: process.env.COOKIE_SECRET,
+    httpOnly: true
+  })
+);
+
+// routes router
 app.use("/", router);
 
 app.listen(port, () => {
