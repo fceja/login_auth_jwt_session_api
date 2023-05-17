920502460cad9ec34a3d80ef7ab9259c9db9167a
FC <fceja@FCs-MacBook-Pro.lan>
2023-05-17 14:21:30 -0700

    minor update

--- app/middleware/index.ts
+++ app/middleware/index.ts
@@ -2,13 +2,17 @@ import refreshJwtToken from './auth/refresJwtToken';
 import validateJwtToken from './auth/validateJwtToken';
 import { loginAuth } from "../middleware/auth/loginAuth";
 
+import { someHandler } from "../middleware/test/testHandler";
+
 import { createUser } from "../middleware/user/createUser";
 import { getUsers } from "../middleware/user/getUsers";
+
 
 export const midWare = {
     authRefreshJwtToken:refreshJwtToken,
     authValidateJwtToken:validateJwtToken,
     authLoginAuth:loginAuth,
+    testSomeHandler:someHandler,
     userCreateUser:createUser,
     userGetUsers:getUsers
 };
