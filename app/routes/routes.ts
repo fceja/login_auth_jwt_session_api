50d5d944f3fa21e9b689a93c4b91b1fd472186ca
fceja <cisco_ceja@icloud.com>
2023-05-17 09:11:48 -0700

    add token validation

--- app/routes/routes.ts
+++ app/routes/routes.ts
@@ -1,14 +1,16 @@
-import express, { Router } from "express";
+import express from "express";
 
 import userRouter from "./userRoutes";
 import authRouter from "./authRoutes";
 import testRouter from "./testRoutes";
+import validateJwt from "../middleware/validateJwt";
 
-const router: Router = express.Router();
+// init
+const router = express.Router();
 
+// routes
 router.get("/", (req, res) => {res.json({message:'index'})});
-
-router.use("/test", testRouter);
+router.use("/test", [validateJwt], testRouter);
 router.use("/auth", authRouter);
 router.use("/user", userRouter);
 
