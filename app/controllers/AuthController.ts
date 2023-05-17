da76cbe78d6b9a3a2468abded4a3544ca47f499b
fceja <cisco_ceja@icloud.com>
2023-05-17 10:38:07 -0700

    formatting

--- app/controllers/AuthController.ts
+++ app/controllers/AuthController.ts
@@ -2,44 +2,45 @@ import dbPool from "../utils/dbInit";
 
 const authConfig = require("../config/authConfig");
 const bcrypt = require("bcrypt");
-const dbService = require("../services/dbUserService")
+const dbService = require("../services/dbUserService");
 const jwt = require("jsonwebtoken");
 
 exports.loginAuth = async (user, req, res) => {
-    // init db connection
-    const dbConn = await dbPool.connect();
+  // init db connection
+  const dbConn = await dbPool.connect();
 
-    // check if email already exists
-    const emailExists = await dbService.ifUserExists(dbConn, user.email);
-    if (!emailExists) {
-      // email exists, terminate db connection
-      dbConn.end();
+  // check if email already exists
+  const emailExists = await dbService.ifUserExists(dbConn, user.email);
+  if (!emailExists) {
+    // email exists, terminate db connection
+    dbConn.end();
 
-      return false;
-    }
-
-    // query db with user email
-    const query = await dbConn.query(
-      `select * from "Users" where email='${user.email}'`
-    );
-
-    // check if password is valid
-    const isPassValid = bcrypt.compareSync(user.password, query.rows[0].password);
-    if (!isPassValid) {
-
-      return false
-    }
-
-    // get session jwt token
-    req.session.token = exports.getSessionToken(req, user)
-
-    return true
-  };
-
-  exports.getSessionToken = (req, user) => {
-      // assign jwt token
-      const token = jwt.sign({ id: user.id }, authConfig.secret, {
-        expiresIn: 10, // 10 secs
-      });
-      return token;
+    return false;
   }
+
+  // query db with user email
+  const query = await dbConn.query(
+    `select * from "Users" where email='${user.email}'`
+  );
+
+  // check if password is valid
+  const isPassValid = bcrypt.compareSync(user.password, query.rows[0].password);
+  if (!isPassValid) {
+
+    return false;
+  }
+
+  // get session jwt token
+  req.session.token = exports.getSessionToken(req, user);
+
+  return true;
+};
+
+exports.getSessionToken = (req, user) => {
+  // assign jwt token
+  const token = jwt.sign({ id: user.id }, authConfig.secret, {
+    expiresIn: 10, // 10 secs
+  });
+
+  return token;
+};
