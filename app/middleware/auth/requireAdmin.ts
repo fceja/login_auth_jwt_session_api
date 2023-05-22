cec9f229827024e06fce490edd3b3181008bff19
fceja <cisco_ceja@icloud.com>
2023-05-22 16:23:24 -0700

    add require Admin role to route

---
+++ app/middleware/auth/requireAdmin.ts
@@ -0,0 +1,8 @@
+export default function requireAdminMW (req, res, next) {
+    if (req.session.userRole === 'admin') {
+        next()
+    }
+    else {
+        res.status(403).send({message:'Access denied'});
+    }
+}
