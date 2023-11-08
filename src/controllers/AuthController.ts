0e64d3e7f152fa5ef0e306ab4af9b756c0525bfc
fceja <cisco_ceja@icloud.com>
2023-11-08 15:36:42 -0800

    update AuthController for ts typechecking

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,44 +1,27 @@
-import assert from "assert";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { Request, Response } from "express";
+import { PoolClient } from "pg";
 
+import _SessionData from "../types/express-session/index";
 import { authConfig } from "../config/AuthConfig";
 import dbPool from "../utils/DbInit";
+import User from "../models/User";
 
-const dbGetUserByEmailWithRole = (dbConn, email) => {
-  return dbConn.query(
-    `
-    select *
-    from _user t1
-    left join _user_role t2 on t1.user_id = t2.user_id
-    where t1.email='${email}' and t2.role is not NULL
-    `
-  );
-};
-
-export const getSessionToken = (req: Request, email, userId) => {
-  // assign jwt token
-  const payload = {
-    email: email,
-    userId: userId,
-  };
-  const token = jwt.sign(payload, authConfig.secret, {
-    expiresIn: 10, // 10 secs
-  });
-
-  return token;
-};
-
-export const loginAuth = async (userData, req: Request, res: Response) => {
+export const loginAuth = async (
+  userData: User,
+  req: Request,
+  _resp: Response
+) => {
   // init db connection
   const dbConn = await dbPool.connect();
 
   // get existing user and role
   const retUser = (await dbGetUserByEmailWithRole(dbConn, userData.email))
     .rows[0];
+
   if (!retUser) {
-    dbConn.end();
+    dbConn.release();
 
     return false;
   }
@@ -48,8 +31,6 @@ export const loginAuth = async (userData, req: Request, res: Response) => {
   if (!isPassValid) {
     return false;
   }
-
-  // store user data and token
   req.session.userId = JSON.parse(JSON.stringify(retUser)).user_id;
   req.session.email = JSON.parse(JSON.stringify(retUser)).email;
   req.session.userRole = JSON.parse(JSON.stringify(retUser)).role;
@@ -60,6 +41,30 @@ export const loginAuth = async (userData, req: Request, res: Response) => {
   );
 
   return true;
+};
+
+const dbGetUserByEmailWithRole = (dbConn: PoolClient, email: string) => {
+  return dbConn.query(
+    `
+    select *
+    from _user t1
+    left join _user_role t2 on t1.user_id = t2.user_id
+    where t1.email='${email}' and t2.role is not NULL
+    `
+  );
+};
+
+export const getSessionToken = (email: string, userId: string) => {
+  // assign jwt token
+  const payload = {
+    email: email,
+    userId: userId,
+  };
+  const token = jwt.sign(payload, authConfig.secret, {
+    expiresIn: 10, // 10 secs
+  });
+
+  return token;
 };
 
 export const validateJwtToken = (req: Request) => {
