48ba81cb708c3df5fdb0db465d7b988881ca1555
fceja <cisco_ceja@icloud.com>
2023-11-09 10:00:10 -0800

    add comments and convert to arrow function

--- src/middleware/auth/refresJwtToken.ts
+++ src/middleware/auth/refresJwtToken.ts
@@ -1,11 +1,24 @@
 import { Request, Response, NextFunction } from "express";
 import { getSessionToken } from "../../controllers/AuthController";
 
-export default function refreshJwtTokenMW(
+/**
+ *
+ * @param req: Request
+ * @param _res: Response
+ * @param next: NextFunction
+ *
+ * - Assigns token generated from 'getSessionToken' function to session
+ * - Invokes callback function
+ **/
+
+const refreshJwtTokenMW = (
   req: Request,
   _res: Response,
   next: NextFunction
-) {
+) => {
   req.session.token = getSessionToken(req.session.email, req.session.userId);
+
   next();
-}
+};
+
+export default refreshJwtTokenMW;
