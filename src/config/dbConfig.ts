8945aaf93d3e6330e5f953a0a7ee7be9a01a2c81
fceja <cisco_ceja@icloud.com>
2023-11-08 18:53:05 -0800

    remove vars for db init, not needed

--- src/config/dbConfig.ts
+++ src/config/dbConfig.ts
@@ -3,12 +3,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const dbConfig = {
-  DB_TYPE: process.env.DB_TYPE,
   HOST: process.env.DB_HOST,
+  DB_NAME: process.env.DB_NAME,
   USER: process.env.DB_USER,
   PASSWORD: process.env.DB_PASSWORD,
-  DB_NAME: process.env.DB_NAME,
   PORT: process.env.DB_PORT,
-  dialect: process.env.DB_DIALECT,
-  pool: process.env.DB_POOL,
 };
--- src/utils/dbInit.ts
+++ src/utils/dbInit.ts
@@ -2,9 +2,9 @@ import { dbConfig } from "../config/DbConfig";
 import { Pool } from "pg";
 
 const dbPool = new Pool({
-  user: dbConfig.USER,
   host: dbConfig.HOST,
   database: dbConfig.DB_NAME,
+  user: dbConfig.USER,
   password: dbConfig.PASSWORD,
   port: parseInt(dbConfig.PORT),
 });
